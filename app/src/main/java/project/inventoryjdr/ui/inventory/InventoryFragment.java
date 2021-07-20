880ca106b41c1b730300f73c938500fb0ce2f92c
MaxenceLebard <maxencelebard@mac.com>
2021-07-20 10:56:03 +0200

    Add new screen

---
+++ app/src/main/java/project/inventoryjdr/ui/inventory/InventoryFragment.java
@@ -0,0 +1,34 @@
+package project.inventoryjdr.ui.inventory;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import project.inventoryjdr.R;
+
+public class InventoryFragment extends Fragment {
+    private InventoryViewModel inventoryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        inventoryViewModel =
+                new ViewModelProvider(this).get(InventoryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_inventory, container, false);
+        final TextView textView = root.findViewById(R.id.text_obj);
+        inventoryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
